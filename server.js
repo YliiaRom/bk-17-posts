6b7c7a72fe23ca85aaa9461e892a23197b886c71
Romanenko Yliia(mac) <romany0507306677@gmail.com>
2025-08-25 17:40:35 +0300

    add path

--- server.js
+++ server.js
@@ -1,33 +1,37 @@
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import dotenv from 'dotenv'
-import { delay } from './middleware/delay.js'
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import { delay } from "./middleware/delay.js";
 
-import authRoutes from './routes/auth.js'
-import userRoutes from './routes/users.js'
-import postRoutes from './routes/posts.js'
-import commentRoutes from './routes/comments.js'
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import commentRoutes from "./routes/comments.js";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
 // app.use(cors({ origin: 'http://localhost:5173/', credentials: true }))
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'], // ✅ фронтенд (Vite)
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://brilliant-cobbler-ec51b4.netlify.app",
+    ], // ✅ фронтенд (Vite)
     credentials: true, // ✅ дозвіл надсилати cookie
   })
-)
+);
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(delay)
+app.use(express.json());
+app.use(cookieParser());
+app.use(delay);
 
-app.use('/api/auth', authRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/posts', postRoutes)
-app.use('/api/comments', commentRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
 
-app.listen(4000, () => console.log('API on http://localhost:4000'))
+app.listen(4000, () => console.log("API on http://localhost:4000"));
